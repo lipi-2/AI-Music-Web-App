0f670f5bb19a6b4dc0c6ea90e166fbb07d81a6fe
lipi-2 <97337501+lipi-2@users.noreply.github.com>
2022-01-08 16:46:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+song1 = "";
+song2 = "";
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+    song1 = loadSound("Faded.mp3");
+    song2 = loadSound("Darkside.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+    }
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+}
