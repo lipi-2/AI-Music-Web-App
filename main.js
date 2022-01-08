1cabfd2e0ea4071056ba5fad6f188e1b17dc70aa
lipi-2 <97337501+lipi-2@users.noreply.github.com>
2022-01-08 15:31:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+    song1 = loadSound("Faded.mp3");
+    song2 = loadSound("Darkside.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+}
