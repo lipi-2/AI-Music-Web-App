4010d24bf2a48f85169373c95860ea7728709b90
lipi-2 <97337501+lipi-2@users.noreply.github.com>
2022-01-11 18:12:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function preload()
 function setup()
 {
     canvas = createCanvas(600,500);
-    canvas.center();
+    canvas.position(600,300);
 
     video = createCapture(VIDEO);
     video.hide();
